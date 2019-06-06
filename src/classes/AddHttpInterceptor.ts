a68f62b0fec64f3f1f5177751593993b74d16a25
Anne Castrillon <anne.castrillon@gmail.com>
2019-06-06 02:30:58 -0700

    test

--- src/classes/AddHttpInterceptor.ts
+++ src/classes/AddHttpInterceptor.ts
@@ -24,6 +24,7 @@ export class AddHttpInterceptor implements HttpInterceptor {
     }
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('uhhh..................');
     // Clone the request to add the new header
     if (this.token) {
       const clonedRequest = req.clone({ headers: req.headers.set('Authorization', `Bearer ${this.token}`) });
